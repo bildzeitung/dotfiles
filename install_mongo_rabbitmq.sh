7cd18b4f7fab858b21e6ef9c2cc88b86381b4c95
Daniel Klein <bildzeitung@gmail.com>
2013-05-21 02:23:12 -0400

    Fixed mongo and rabbit install

--- install_mongo_rabbitmq.sh
+++ install_mongo_rabbitmq.sh
@@ -4,12 +4,11 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
 
 
-sudo echo 'deb http://www.rabbitmq.com/debian/ testing main' >> /etc/apt/sources.list
+sudo /bin/sh -c "echo 'deb http://www.rabbitmq.com/debian/ testing main' >> /etc/apt/sources.list"
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 sudo apt-key add rabbitmq-signing-key-public.asc
 rm rabbitmq-signing-key-public.asc
 
-sudo apt-get-update
+sudo apt-get update
 
-sudo apt-get -y install mongodb-10gen
-sudo apt-get -y install rabbitmq-server
+sudo apt-get -y install mongodb-10gen rabbitmq-server
