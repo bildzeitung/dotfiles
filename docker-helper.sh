47cb4a75b2c6fa45c8ebe5fe76bdfd628d0295d6
daniel.klein <daniel.klein@oracle.com>
2016-03-10 16:03:59 -0500

    Added note on starting docker

--- docker-helper.sh
+++ docker-helper.sh
@@ -5,6 +5,7 @@ docker_running=$(docker-machine ls | grep $host)
 if [[ "$docker_running" == *"Stopped"* ]]
 then
     echo "fyi - docker not running"
+    echo "run docker-machine start default"
 elif [[ "$docker_running" == *"Running"* ]]
 then
     eval "$(docker-machine env $host)"
